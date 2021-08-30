afe2af989cef5f0c4668d4dd50403ba093d004c6
underthestars-zhy <zhuhaoyu0909@icloud.com>
2021-08-30 10:40:09 +0800

    Simplify code structure

--- Sources/DictApi/DictApi.swift
+++ Sources/DictApi/DictApi.swift
@@ -28,18 +28,20 @@ public struct DictApi {
 
             let doc: Document = try SwiftSoup.parse(html)
             
-            guard let soundUrlString = try doc.body()?
-                    .getElementsByTag("main")
-                    .first()?
-                    .getElementById("main_content")?
-                    .getElementsByClass("res_cell_center")
-                    .first()?
-                    .getElementsByClass("dc res_cell_center_content")
-                    .first()?
-                    .getElementsByClass("he")
-                    .first()?
-                    .getElementsByClass("cB cB-t")
-                    .first()?
+            let contentElement = try doc.body()?
+                .getElementsByTag("main")
+                .first()?
+                .getElementById("main_content")?
+                .getElementsByClass("res_cell_center")
+                .first()?
+                .getElementsByClass("dc res_cell_center_content")
+                .first()?
+                .getElementsByClass("he")
+                .first()?
+                .getElementsByClass("cB cB-t")
+                .first()
+            
+            guard let soundUrlString = try contentElement?
                     .getElementsByClass("cB-h")
                     .first()?
                     .getAllElements()
@@ -48,18 +50,7 @@ public struct DictApi {
                     .attr("data-src-mp3"),
                 let sound_url = URL(string: soundUrlString) else { return nil }
             
-            guard let allExplain = try doc.body()?
-                    .getElementsByTag("main")
-                    .first()?
-                    .getElementById("main_content")?
-                    .getElementsByClass("res_cell_center")
-                    .first()?
-                    .getElementsByClass("dc res_cell_center_content")
-                    .first()?
-                    .getElementsByClass("he")
-                    .first()?
-                    .getElementsByClass("cB cB-t")
-                    .first()?
+            guard let allExplain = try contentElement?
                     .getElementsByClass("hom").array() else { return nil }
             
             var psArray = [String]()
